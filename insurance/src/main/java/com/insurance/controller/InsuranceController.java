d67cf4d591dba7086049e9c2be42aba0aa7b1800
kuldeepjha <kuldeepjha09@gmail.com>
2018-07-18 22:45:33 +0530

    insurance:file

---
+++ insurance/src/main/java/com/insurance/controller/InsuranceController.java
@@ -0,0 +1,13 @@
+package com.insurance.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InsuranceController {
+
+	@RequestMapping("/")
+	public String getContect() {
+		return "contect";
+	}
+}
